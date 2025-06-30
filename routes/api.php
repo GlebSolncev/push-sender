983bf228f7be576c470e77558cb3970863bf5c4f
Gleb Solncev <solntsev.gleb@planetvpn.dev>
2025-06-30 23:00:02 +0300

    init

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PushController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('push')->group(function () {
-    Route::put('subscribe', [PushController::class, 'subscribe']);
+    Route::post('subscribe', [PushController::class, 'subscribe']);
 
     Route::post('statistic', [PushController::class, 'statistic']);
 });
