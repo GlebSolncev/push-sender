64c6c92c0b626ca92d359e7c109e42c9acc46730
Gleb Solncev <solntsev.gleb@planetvpn.dev>
2025-06-30 23:05:56 +0300

    init

--- app/Jobs/SendPushNotification.php
+++ app/Jobs/SendPushNotification.php
@@ -25,54 +25,57 @@ class SendPushNotification implements ShouldQueue
 
     /**
      * Execute the job.
+     * @throws \ErrorException|\JsonException
      */
     public function handle(): void
     {
-        $subs = PushSubscription::query()->first();
+        $subscribers = PushSubscription::query()->get();
 
-        $subscription = Subscription::create([
-            'endpoint' => $subs->endpoint,
-            'publicKey' => $subs->p256dh,
-            'authToken' => $subs->auth,
-            'contentEncoding' => self::ENCODE,
-        ]);
-
-
-        $webPush = new WebPush([
-            'VAPID' => [
-                'subject' => 'mailto:admin',
-                'publicKey' => config('vapid.public_key'),
-                'privateKey' => config('vapid.private_key'),
-            ],
-        ]);
-
-        $image = null;
-        $icon = null;
-
-        if($this->message->icon) {
-            $icon = Storage::disk('s3')->url($this->message->icon);
-        }
-
-        if(!$icon) {
-            if($this->message->image) {
-                $image = Storage::disk('s3')->url($this->message->image);
-            }
-        }
+        foreach($subscribers as $inx => $subscriber) {
+            $subscription = Subscription::create([
+                'endpoint' => $subscriber->endpoint,
+                'publicKey' => $subscriber->p256dh,
+                'authToken' => $subscriber->auth,
+                'contentEncoding' => self::ENCODE,
+            ]);
 
 
-        $report = $webPush->sendOneNotification(
-            $subscription,
-            json_encode([
-                'title' => $this->message->title,
-                'icon'  => $icon,
-                'image' => $image,
-                'body'  => $this->message->body,
-                'data'  => [
-                    'url' => $this->message->link,
-                    'id'  => 1,
+            $webPush = new WebPush([
+                'VAPID' => [
+                    'subject' => 'mailto:admin',
+                    'publicKey' => config('vapid.public_key'),
+                    'privateKey' => config('vapid.private_key'),
                 ],
-            ], JSON_THROW_ON_ERROR)
-        );
+            ]);
+
+            $image = null;
+            $icon = null;
+
+            if($this->message->icon) {
+                $icon = Storage::disk('s3')->url($this->message->icon);
+            }
+
+            if(!$icon) {
+                if($this->message->image) {
+                    $image = Storage::disk('s3')->url($this->message->image);
+                }
+            }
+
+
+            $report = $webPush->sendOneNotification(
+                $subscription,
+                json_encode([
+                    'title' => $this->message->title . ' - ' . $inx,
+                    'icon'  => $icon,
+                    'image' => $image,
+                    'body'  => $this->message->body,
+                    'data'  => [
+                        'url' => $this->message->link,
+                        'id'  => 1,
+                    ],
+                ], JSON_THROW_ON_ERROR)
+            );
+        }
 
 
         $reason = $report->getReason();
